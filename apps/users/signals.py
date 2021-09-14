9c061a9b7193d04ca14b44d96fdc8e21d3d1a9bc
Francisco Del Roio <francipvb@hotmail.com>
2021-09-14 09:53:01 -0300

    Finished project configuration:
    * Added allauth and rest-auth applications.
    * Hooked the URLs to these endpoints.

---
+++ apps/users/signals.py
@@ -0,0 +1,15 @@
+from typing import Any, Type
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from .models import User
+
+
+@receiver(pre_save, sender=User)
+def _create_superuser(sender: Type[User], instance: User, *args: Any, **kwargs: Any) -> None:
+    if kwargs["raw"]:
+        return
+
+    if not User.objects.all().exists():
+        instance.is_superuser = True
+        instance.is_staff = True
